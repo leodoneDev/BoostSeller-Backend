a8b3be424a7185549171aaaf1613359fa70d6d9c
leodoneDev <leodone125@gmail.com>
2025-05-09 20:42:58 +0700

    expire error exception

--- app/api/auth/verify-otp/route.ts
+++ app/api/auth/verify-otp/route.ts
@@ -16,16 +16,16 @@ export async function POST(req: Request) {
         });
     
         if (!otpRecord) {
-          return new Response(JSON.stringify({ error: true, message: 'Invalid or expired OTP.' }), {});
+          return new Response(JSON.stringify({ error: true, message: 'Invalid or expired OTP.', expire: true }), {});
         }
     
         // Optional: delete OTP after successful use
         await prisma.otp.delete({ where: { id: otpRecord.id } });
     
-        return new Response(JSON.stringify({ error: false, message: 'OTP verified!' }), { status: 200 });
+        return new Response(JSON.stringify({ error: false, message: 'OTP verified!', expire: false, }), { status: 200 });
       } catch (err) {
         console.error(err);
-        return new Response(JSON.stringify({ error: true, message: 'OTP verification failed. \n Please try again.' }), {});
+        return new Response(JSON.stringify({ error: true, message: 'OTP verification failed. \n Please try again.', expire: false }), {});
       }
   } else {
 
