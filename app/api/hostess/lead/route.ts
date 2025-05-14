1ecdb3ab92938439622be8a52de87dcfacd5d507
leodoneDev <leodone125@gmail.com>
2025-05-14 22:21:05 +0700

    order change again

--- app/api/hostess/lead/route.ts
+++ app/api/hostess/lead/route.ts
@@ -23,7 +23,7 @@ export async function POST(req: Request) {
           createdAt:'desc',
         },
       });
-
+      console.log(leads);
     return new Response(JSON.stringify({
       error: false,
       leads,
