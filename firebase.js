ec80ae942dd53ab64d94943887f715d175774880
leodoneDev <leodone125@gmail.com>
2025-05-21 19:39:05 +0700

    fix issue

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require("./serviceAccount.json");
 
 // const serviceAccount = {
 //   type: process.env.FIREBASE_TYPE,
