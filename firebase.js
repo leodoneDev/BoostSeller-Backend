ff935ba77eed912a082118df3232c8b6e6bb8ca0
leodoneDev <leodone125@gmail.com>
2025-05-21 22:03:43 +0700

    deploy for railway

--- firebase.js
+++ firebase.js
@@ -1,22 +1,12 @@
 
 const admin = require("firebase-admin");
 
-if (!admin.apps.length) {
-  let serviceAccount;
+serviceAccount = require('./config/firebase-adminsdk.json');
+  
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
-  if (process.env.NODE_ENV === "production") {
-    // In production (like Railway), decode base64 env variable
-    const serviceAccountJson = Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, 'base64').toString('utf8');
-    serviceAccount = JSON.parse(serviceAccountJson);
-  } else {
-    // In development (your local machine), read from file
-    serviceAccount = require('./config/firebase-adminsdk.json');
-  }
-
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-}
 
 const sendPushNotification = async (fcmToken, data) => {
   await admin.messaging().send({
