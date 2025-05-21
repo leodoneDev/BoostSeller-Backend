e21eace0a0e3e21e53cf1c6c86515e88ce3259df
leodoneDev <leodone125@gmail.com>
2025-05-21 21:11:47 +0700

    update

--- firebase.js
+++ firebase.js
@@ -1,6 +1,23 @@
 
 const admin = require("firebase-admin");
 
+if (!admin.apps.length) {
+  let serviceAccount;
+
+  if (process.env.NODE_ENV === "production") {
+    // In production (like Railway), decode base64 env variable
+    const serviceAccountJson = Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, 'base64').toString('utf8');
+    serviceAccount = JSON.parse(serviceAccountJson);
+  } else {
+    // In development (your local machine), read from file
+    serviceAccount = require('./config/firebase-adminsdk.json');
+  }
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
 const serviceAccount = require("./config/serviceAccount.json");
 
 admin.initializeApp({
