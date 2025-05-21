6a34ccc4b88d4d51ab50765546c6752544f6f2a3
leodoneDev <leodone125@gmail.com>
2025-05-21 19:35:28 +0700

    add serviceAccount.json

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./config/serviceAccountKey.json");
+const serviceAccount = require("./serviceAccountKey.json");
 
 // const serviceAccount = {
 //   type: process.env.FIREBASE_TYPE,
