4d9bab1ccb3ef833eb630a1e1522790b5161c255
leodoneDev <leodone125@gmail.com>
2025-05-22 23:42:43 +0700

    port update

--- app/api/auth/send-otp/route.ts
+++ app/api/auth/send-otp/route.ts
@@ -18,7 +18,7 @@ export async function POST(req: Request) {
 
         const transporter = nodemailer.createTransport({
           host: process.env.SMTP_HOST,
-          port: process.env.SMTP_PORT,
+          port: 465,
           secure: true, 
           auth: {
             user: process.env.SMTP_USER,
