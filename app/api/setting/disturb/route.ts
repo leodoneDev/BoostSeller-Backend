e401a68cbf6cc9a429956b5a01f9e861d9321900
leodoneDev <leodone125@gmail.com>
2025-05-26 14:17:08 +0700

    disturb mode update

--- app/api/setting/disturb/route.ts
+++ app/api/setting/disturb/route.ts
@@ -11,7 +11,7 @@ export async function POST(req: Request) {
             id: paresedPerformerId,
         },
         data: {
-            available: flag,
+            available: !flag,
         },
     });
     
